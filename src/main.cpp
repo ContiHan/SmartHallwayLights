93d29340812bfe34c3162e19c02c4ff9e2e9890e
Daniel Hanák <daniel.hanak@outlook.cz>
2024-04-28 18:56:29 +0200

    added OTA flashing

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include "Arduino.h"
+#include "ArduinoOTA.h"
 
 #define PWM_CHANNEL_0 0
 #define PWM_TIMER_13_BIT 13
@@ -39,6 +40,7 @@ void setTimeClient();
 String elapsedTimeHtml();
 void startWebServer();
 void setupMDNS();
+void initArduinoOTA();
 
 void setup()
 {
@@ -51,6 +53,43 @@ void setup()
   setServerResponses();
   setTimeClient();
   startWebServer();
+  initArduinoOTA();
+}
+
+void initArduinoOTA()
+{
+  ArduinoOTA
+      .onStart([]()
+               {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      } });
+
+  ArduinoOTA.begin();
 }
 
 void startWebServer()
@@ -84,6 +123,7 @@ void initTasks()
 
 void setWifiConnection()
 {
+  WiFi.mode(WIFI_STA);
   WiFi.begin(wifiSSID, wifiPassword);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -141,7 +181,7 @@ void mainHtmlMessage()
                        "</style>"
                        "</head>"
                        "<body>"
-                       "<h1>Ahoj Arduino světe!</h1>"
+                       "<h1>Chytré osvětlení chodby</h1>"
                        "<p>Čas od spuštění je " +
                        timeFromStart + "</p>"
                                        "<p>PWM nastaveno na: " +
@@ -229,4 +269,5 @@ void cyclePWMTask(void *parameter)
 void loop()
 {
   server.handleClient();
+  ArduinoOTA.handle();
 }
